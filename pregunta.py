b4a1355881304629f9ca59e101825382fe223601
LuisFGarzon <lfgarzona@unal.edu.co>
2022-10-04 22:45:02 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,27 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
+    df = pd.read_csv('solicitudes_credito.csv',sep=";",index_col=0,encoding="utf8")
+    df.dropna(inplace=True)
+    for j in df.columns[0:4]:
+        df[j]=df[j].str.lower()
+    df["línea_credito"]=df.línea_credito.str.lower()
+    df['fecha_de_beneficio']=pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+    df["monto_del_credito"]=[line.replace("$","") for line in df["monto_del_credito"]]
+    df["monto_del_credito"]=[line.replace(",","") for line in df["monto_del_credito"]]
+    df["monto_del_credito"]=[line.replace(".00","") for line in df["monto_del_credito"]]
+    df["monto_del_credito"]=df["monto_del_credito"].astype("float")
+    df["barrio"]=df["barrio"].astype("str")
+    df["barrio"]=[line.replace(" ","_") for line in df["barrio"]]
+    df["barrio"]=[line.replace("-","_") for line in df["barrio"]]
+    df["barrio"]=[line.replace("_"," ") for line in df["barrio"]]
+    df["idea_negocio"]=df["idea_negocio"].astype("str")
+    df["idea_negocio"]=[line.replace(" ","_") for line in df["idea_negocio"]]
+    df["idea_negocio"]=[line.replace("-","_") for line in df["idea_negocio"]]
+    df["idea_negocio"]=[line.replace("_"," ") for line in df["idea_negocio"]]
+    df["línea_credito"]=[line.replace(" ","_") for line in df["línea_credito"]]
+    df["línea_credito"]=[line.replace("-","_") for line in df["línea_credito"]]
+    df["línea_credito"]=[line.replace("_"," ") for line in df["línea_credito"]]
+    df.drop_duplicates(inplace=True)
 
     return df
